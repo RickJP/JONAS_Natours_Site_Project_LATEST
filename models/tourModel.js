365c479e0852d681c8d70b05e2979f59eaf2caa4
Rick <{ID}+{username}@users.noreply.github.com>
2019-07-24 18:25:41 +0900

    ADD: Document Middleware - slug for name

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -6,6 +7,9 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
     trim: true
+  },
+  slug: {
+    type: String
   },
   duration: {
     type: Number,
@@ -61,5 +65,21 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before the save command & .create command
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log("Will save document.");
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 module.exports = mongoose.model('Tour', tourSchema);
