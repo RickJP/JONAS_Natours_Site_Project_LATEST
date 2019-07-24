c1a6fe19bac6c59ddb049e74b0588e9b38ef30bf
Rick <{ID}+{username}@users.noreply.github.com>
2019-07-24 23:31:35 +0900

    ADD: Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,7 +55,11 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(),
     select: false
   },
-  startDates: [Date]
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
+  }
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
@@ -81,5 +85,18 @@ tourSchema.pre('save', function(next) {
 //   console.log(doc);
 //   next();
 // });
+
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: {$ne: true}});
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds.`)
+  console.log(docs);
+  next();
+});
 
 module.exports = mongoose.model('Tour', tourSchema);
