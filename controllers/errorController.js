d6da3d1d1f2fd427740d5d4edb633e3570344854
Rick <{ID}+{username}@users.noreply.github.com>
2019-08-10 22:26:23 +0900

    ADD: Handle mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,19 @@ const handleCastErrorDB = err => {
 	return new AppError(msg, 400);
 };
 
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  
+  const msg = `Duplicate field value: ${value}, Please use another value!`;
+  return new AppError(msg, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data ${errors.join('. ')}`;
+  return new AppError(message, 400);
+}
+
 const sendErrDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -12,14 +25,6 @@ const sendErrDev = (err, res) => {
     message: err.message,
     stack: err.stack
   });
-};
-
-const handleDuplicateFieldsDB = err => {
-  console.log('####');
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  
-  const msg = `Duplicate field value: ${value}, Please use another value!`;
-  return new AppError(msg, 400);
 };
 
 const sendErrProd = (err, res) => {
@@ -53,8 +58,8 @@ module.exports = (err, req, res, next ) => {
     
 		if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    // if (error.name === 'ValidationError')
-    //   error = handleValidationErrorDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     sendErrProd(error, res);
   }
 };
