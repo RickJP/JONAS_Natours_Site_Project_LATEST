a72978788281718304de767b889c03f9888b1c6a
Rick <{ID}+{username}@users.noreply.github.com>
2019-08-11 18:58:53 +0900

    ADD: User Model

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const validation = require('validator');
+
+const userSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true, 'A username is required.'],
+    unique: true,
+    trim: true,
+    minlength: [6, 'A username must contain 6 or more charcaters'],
+    maxlength: [30, 'A username must contain 30 characters or less']
+  },
+  email: {
+    type: String,
+    required: [true, 'An email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'A password is required.'],
+    unique: true,
+    trim: true,
+    minlength: [6, 'A password must contain 6 or more characters'],
+    maxlength: [30, 'A password must contain 30 characters or less']
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
