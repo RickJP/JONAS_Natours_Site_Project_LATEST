7d05b74d099a65b9dfd505b40f215f72a074a59d
Rick <{ID}+{username}@users.noreply.github.com>
2019-08-12 09:31:58 +0900

    ADD: Hash password & password comfirm check

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema ({
   name: {
@@ -21,19 +22,35 @@ const userSchema = new mongoose.Schema ({
   photo: String,
   password: {
     type: String,
-    required: [true, 'A password is required.'],
-    unique: true,
     trim: true,
-    minlength: [6, 'A password must contain 6 or more characters'],
-    maxlength: [30, 'A password must contain 30 characters or less']
+    required: [true, 'A password is required.'],
+    minlength: 6
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    trim: true,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE & SAVE
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same'
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
