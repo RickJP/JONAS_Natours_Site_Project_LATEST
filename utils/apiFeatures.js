3f05e5ef22deef5dc6459f8116e1d54a2b31f0ed
Rick <{ID}+{username}@users.noreply.github.com>
2019-08-24 16:48:40 +0900

    ADD: Update Tour model to handle geoJSON data

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -19,7 +19,7 @@ class APIFeatures {
 
   sort() {
     if (this.queryString.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
+      const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-createdAt');
@@ -43,7 +43,6 @@ class APIFeatures {
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
-
     return this;
   }
 }
