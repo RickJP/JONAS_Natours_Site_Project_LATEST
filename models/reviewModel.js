fad27af073b62f72cffb4b0a7c8db30996424075
Rick <{ID}+{username}@users.noreply.github.com>
2019-08-25 10:23:09 +0900

    ADD: Virtual Populate - Tours & Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -31,14 +31,19 @@ const reviewSchema = new mongoose.Schema({
 });
 
 reviewSchema.pre(/^find/, function(next) {
+  // this.populate({
+  //   path: 'tour',
+  //   select: 'name'
+  // }).populate({
+  //   path: 'user',
+  //   select: 'name photo'
+  // });
+  // next();
+
   this.populate({
-    path: 'tour',
-    select: 'name'
-  }).populate({
     path: 'user',
     select: 'name photo'
   });
-
   next();
 });
 
