df20396911050982f509d6bdb907ab9635b28606
Rick <{ID}+{username}@users.noreply.github.com>
2019-11-30 13:26:49 +0900

    ADD: Finished settinging up Stripe payment functionality

---
+++ public/js/stripe.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts.js';
+const stripe = Stripe('pk_test_QtcBKPRHkF7O6y8md2OX0LPR');
+
+export const bookTour = async tourId => {
+  // Get  checkout session from API
+  try {
+    const session = await axios(`http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`);
+    // Create checkout form + charge credit card
+    console.log(session);
+
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+
+  } catch  (err){
+      console.log(err);
+      showAlert('error', err);
+  }
+ 
+}
