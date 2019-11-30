d1cd18de3acff235e94ae2f3bd7fc3db8761aea9
Rick <{ID}+{username}@users.noreply.github.com>
2019-11-30 21:42:22 +0900

    Latest bug fix attempt

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const path = require('path');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
-const viewRouter = require('./routes/viewRoutes');
-const bookingRouter = require('./routes/bookingRoutes');
-const bookingController = require('./controllers/bookingController');
-
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+const viewRouter = require('./routes/viewRoutes');
+
+// Start express app
 const app = express();
 
 app.enable('trust proxy');
 
-// TEMPLATE ENGINE
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// GLOBAL MIDDLEWARE
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
 app.use(cors());
 // Access-Control-Allow-Origin *
-
+// api.natours.com, front-end natours.com
 // app.use(cors({
 //   origin: 'https://www.natours.com'
-// }));
+// }))
 
 app.options('*', cors());
-
 // app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set Security HTTP Headers
+// Set security HTTP headers
 app.use(helmet());
 
 // Development logging
@@ -51,19 +51,24 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Limit request from same IP
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP. Please try again in an hour.'
+  message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
 
-app.post('/webhook-checkout', express.raw({type: 'application/json'}), bookingController.webhookCheckout);
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
-// Body parser - reading data from body into req.body
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded( { extended: true, limit: '10kb' } ));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -88,14 +93,14 @@ app.use(
 
 app.use(compression());
 
-// TEST Middleware
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  //console.log(req.cookies);
+  // console.log(req.cookies);
   next();
 });
 
-// ROUTES
+// 3) ROUTES
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
@@ -103,7 +108,7 @@ app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server.`));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
